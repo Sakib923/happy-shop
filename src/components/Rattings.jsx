5802a0a5f8cb15bbbf6a28319b2f40bf9ce284cb
Sakib <sakibulalam923@gmail.com>
2024-08-03 13:10:02 +0600

    Category shown list updated all working fine

---
+++ src/components/Rattings.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Rattings() {
+  return (
+    <span className="rattingg">
+        <i className="icofont-ui-rating"></i>
+        <i className="icofont-ui-rating"></i>
+        <i className="icofont-ui-rating"></i>
+        <i className="icofont-ui-rating"></i>
+        <i className="icofont-ui-rating"></i>
+    </span>
+  )
+}
+
+export default Rattings;
