5ac3844053e142c6c8e7efc0895d64344eeba986
Sakib <sakibulalam923@gmail.com>
2024-08-03 18:15:37 +0600

    completed home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 
-import './App.css'
-import { Outlet } from 'react-router-dom'
-import NavItem from './components/NavBar/NavItem'
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import NavItem from "./components/NavBar/NavItem";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <NavItem />
-        <Outlet />
+      <NavItem />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
