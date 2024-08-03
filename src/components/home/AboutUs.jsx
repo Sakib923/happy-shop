932c1ad0108e586775eb413669d23ab14c354a0f
Sakib <sakibulalam923@gmail.com>
2024-08-03 16:38:19 +0600

    completed about us card

--- src/components/home/AboutUs.jsx
+++ src/components/home/AboutUs.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import CountUp from "react-countup";
+import { Link } from "react-router-dom";
+import image from "../../assets/images/instructor/01.png";
 
 const subTitle = "Why Choose Us";
 const title = "Become a Marchant";
@@ -25,7 +28,55 @@ const countList = [
 ];
 
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div className="instructor-section style-2 padding-tb section-bg-ash">
+        <div className="container">
+            <div className="section-wrapper">
+                <div className="row g-4 justify-content-center align-items-center row-cols-1 row-cols-md-2
+                row-cols-xl-3 ">
+
+                    <div className="col">
+                        {
+                            countList.map((item, index) => (
+                                <div className="count-item" key={index}>
+                                    <div className="count-inner">
+                                        <div className="count-icon">
+                                            <i className={item.iconName}></i>
+                                        </div>
+                                        <div className="count-content">
+                                            <h2>
+                                                <span className="count"><CountUp end={item.count} /></span>
+                                                <span>+</span>
+                                            </h2>
+                                            <p>{item.text}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                    <div className="col">
+                        <div className="instructor-content">
+                            <span className="subtitle">{subTitle}</span>
+                            <h2 className="title">{title}</h2>
+                            <p>{desc}</p>
+                            <Link to="/sign-up" className="lab-btn"> {btnText}</Link>
+                        </div>
+                    </div>
+
+                    <div className="col ">
+                        <div className="instructor-thumb">
+                            <img src={image} alt="" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+  );
 }
 
 export default AboutUs;
